d74e12a0b9ccb4f656a3c302ac0ac17db94dd9ad
Björn Ellensohn <bjoern.ellensohn@gmail.com>
2023-08-04 13:48:32 +0200

    update: new teleop layout

--- launch/robot_navigation.launch.py
+++ launch/robot_navigation.launch.py
@@ -28,7 +28,7 @@ from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
     # Get the launch directory
-    bringup_dir = get_package_share_directory('nav2_bringup')
+    bringup_dir = get_package_share_directory('cps_rmp220_support')
 
     namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
