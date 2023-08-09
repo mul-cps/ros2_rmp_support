4842f5172a156219de77fe172fe55c983f96b9bf
Björn Ellensohn <bjoern.ellensohn@gmail.com>
2023-08-09 11:50:17 +0200

    update: controller params

--- launch/robot_controller.launch.py
+++ launch/robot_controller.launch.py
@@ -10,7 +10,8 @@ def generate_launch_description():
             package='segwayrmp',
             executable='SmartCar',
             name='SmartCar',
-            remappings=[('cmd_vel','cmd_vel_out'), ('/odom','odom'), ('/imu','imu'), ('/bms_fb','bms_fb'), ('/joint_states','joint_states'), ('/tf','tf'), ('/tf_static','tf_static')],
+            #remappings=[('cmd_vel','cmd_vel_out'), ('/odom','odom'), ('/imu','imu'), ('/bms_fb','bms_fb'), ('/joint_states','joint_states'), ('/tf','tf'), ('/tf_static','tf_static')],
+            remappings=[('cmd_vel','cmd_vel_out')],
             #namespace = "/rmp"
          )
 
