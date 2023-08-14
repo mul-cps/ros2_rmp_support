c8298542ac3d645d37ed768334cc3be3f4e7d1d6
Björn Ellensohn <bjoern.ellensohn@gmail.com>
2023-08-14 08:52:56 +0200

    update

--- launch/robot_joystick.launch.py
+++ launch/robot_joystick.launch.py
@@ -54,7 +54,6 @@ def generate_launch_description():
     teleop_node = Node(
             package='teleop_twist_joy',
             executable='teleop_node',
-            name='teleop_twist_joy',
             parameters=[joy_params, {'use_sim_time': use_sim_time}],
             remappings=[('cmd_vel', 'cmd_vel_joy')],
             #namespace = namespace
