087f3ef1008b5d948cd02fda1790d696f4c6cb50
Your Name <you@example.com>
2025-06-20 09:18:36 +0200

    update femto mega launch file

--- launch/femto_mega.launch.py
+++ launch/femto_mega.launch.py
@@ -13,113 +13,141 @@ compr_jpeg = {"format": "jpeg", "jpeg_quality": 95}
 compr_png = {"format": "png", "png_level": 1}
 
 def generate_launch_description():
-    # Launch arguments
-    camera_name = LaunchConfiguration("camera_name")
-    enable_colored_pc = LaunchConfiguration("enable_colored_point_cloud")
-    enumerate_net_device = LaunchConfiguration("enumerate_net_device")
+    # Declare arguments
+    args = [
+        DeclareLaunchArgument('camera_name', default_value='camera'),
+        DeclareLaunchArgument('depth_registration', default_value='false'),
+        DeclareLaunchArgument('serial_number', default_value=''),
+        DeclareLaunchArgument('usb_port', default_value=''),
+        DeclareLaunchArgument('net_device_ip', default_value=''),
+        DeclareLaunchArgument('net_device_port', default_value='0'),
+        DeclareLaunchArgument('device_num', default_value='1'),
+        DeclareLaunchArgument('vendor_id', default_value='0x2bc5'),
+        DeclareLaunchArgument('product_id', default_value=''),
+        DeclareLaunchArgument('enable_point_cloud', default_value='true'),
+        DeclareLaunchArgument('enable_colored_point_cloud', default_value='false'),
+        DeclareLaunchArgument('cloud_frame_id', default_value=''),
+        DeclareLaunchArgument('point_cloud_qos', default_value='default'),
+        DeclareLaunchArgument('connection_delay', default_value='100'),
+        DeclareLaunchArgument('color_width', default_value='1280'),
+        DeclareLaunchArgument('color_height', default_value='720'),
+        DeclareLaunchArgument('color_fps', default_value='30'),
+        DeclareLaunchArgument('color_format', default_value='MJPG'),
+        DeclareLaunchArgument('enable_color', default_value='true'),
+        DeclareLaunchArgument('flip_color', default_value='false'),
+        DeclareLaunchArgument('color_qos', default_value='default'),
+        DeclareLaunchArgument('color_camera_info_qos', default_value='default'),
+        DeclareLaunchArgument('enable_color_auto_exposure', default_value='true'),
+        DeclareLaunchArgument('color_exposure', default_value='-1'),
+        DeclareLaunchArgument('color_gain', default_value='-1'),
+        DeclareLaunchArgument('enable_color_auto_white_balance', default_value='true'),
+        DeclareLaunchArgument('color_white_balance', default_value='-1'),
+        DeclareLaunchArgument('depth_width', default_value='640'),
+        DeclareLaunchArgument('depth_height', default_value='576'),
+        DeclareLaunchArgument('depth_fps', default_value='30'),
+        DeclareLaunchArgument('depth_format', default_value='Y16'),
+        DeclareLaunchArgument('enable_depth', default_value='true'),
+        DeclareLaunchArgument('flip_depth', default_value='false'),
+        DeclareLaunchArgument('depth_qos', default_value='default'),
+        DeclareLaunchArgument('depth_camera_info_qos', default_value='default'),
+        DeclareLaunchArgument('ir_width', default_value='640'),
+        DeclareLaunchArgument('ir_height', default_value='576'),
+        DeclareLaunchArgument('ir_fps', default_value='30'),
+        DeclareLaunchArgument('ir_format', default_value='Y16'),
+        DeclareLaunchArgument('enable_ir', default_value='true'),
+        DeclareLaunchArgument('flip_ir', default_value='false'),
+        DeclareLaunchArgument('ir_qos', default_value='default'),
+        DeclareLaunchArgument('ir_camera_info_qos', default_value='default'),
+        DeclareLaunchArgument('enable_ir_auto_exposure', default_value='true'),
+        DeclareLaunchArgument('ir_exposure', default_value='-1'),
+        DeclareLaunchArgument('ir_gain', default_value='-1'),
+        DeclareLaunchArgument('enable_sync_output_accel_gyro', default_value='true'),
+        DeclareLaunchArgument('enable_accel', default_value='false'),
+        DeclareLaunchArgument('accel_rate', default_value='100hz'),
+        DeclareLaunchArgument('accel_range', default_value='4g'),
+        DeclareLaunchArgument('enable_gyro', default_value='false'),
+        DeclareLaunchArgument('gyro_rate', default_value='100hz'),
+        DeclareLaunchArgument('gyro_range', default_value='1000dps'),
+        DeclareLaunchArgument('liner_accel_cov', default_value='0.01'),
+        DeclareLaunchArgument('angular_vel_cov', default_value='0.01'),
+        DeclareLaunchArgument('publish_tf', default_value='true'),
+        DeclareLaunchArgument('tf_publish_rate', default_value='0.0'),
+        DeclareLaunchArgument('ir_info_url', default_value=''),
+        DeclareLaunchArgument('color_info_url', default_value=''),
+        # Network device settings: default net_device_ip is 192.168.1.10 and net_device_port is 8090
+        # If you don't want to use ip addr and port, set enumerate_net_device to true
+        # and leave net_device_ip blank , it can enumerate automate
+        DeclareLaunchArgument('enumerate_net_device', default_value='false'),
+        DeclareLaunchArgument('net_device_ip', default_value=''),
+        DeclareLaunchArgument('net_device_port', default_value='0'),
+        DeclareLaunchArgument('log_level', default_value='none'),
+        DeclareLaunchArgument('enable_publish_extrinsic', default_value='false'),
+        DeclareLaunchArgument('enable_d2c_viewer', default_value='false'),
+        DeclareLaunchArgument('enable_ldp', default_value='true'),
+        DeclareLaunchArgument('enable_soft_filter', default_value='true'),
+        DeclareLaunchArgument('soft_filter_max_diff', default_value='-1'),
+        DeclareLaunchArgument('soft_filter_speckle_size', default_value='-1'),
+        DeclareLaunchArgument('sync_mode', default_value='standalone'),
+        DeclareLaunchArgument('depth_delay_us', default_value='0'),
+        DeclareLaunchArgument('color_delay_us', default_value='0'),
+        DeclareLaunchArgument('trigger2image_delay_us', default_value='0'),
+        DeclareLaunchArgument('trigger_out_delay_us', default_value='0'),
+        DeclareLaunchArgument('trigger_out_enabled', default_value='false'),
+        DeclareLaunchArgument('enable_frame_sync', default_value='true'),
+        DeclareLaunchArgument('ordered_pc', default_value='false'),
+        DeclareLaunchArgument('use_hardware_time', default_value='false'),
+        DeclareLaunchArgument('enable_depth_scale', default_value='true'),
+        DeclareLaunchArgument('align_mode', default_value='HW'),
+        DeclareLaunchArgument('laser_energy_level', default_value='-1'),
+        DeclareLaunchArgument('enable_heartbeat', default_value='false'),
+    ]
 
-    declare_camera_name = DeclareLaunchArgument(
-        "camera_name", default_value="femto_mega", description="Camera name"
-    )
-    declare_enable_colored_pc = DeclareLaunchArgument(
-        "enable_colored_point_cloud", default_value="False", description="Enable RGB point cloud"
-    )
-    declare_enumerate_net_device = DeclareLaunchArgument(
-        "enumerate_net_device", default_value="False", description="Search for network devices"
-    )
-
-    femto = ComposableNode(
-        package="orbbec_camera",
-        plugin="orbbec_camera::OBCameraNodeDriver",
-        name="camera",
-        namespace="camera",
-        remappings=[
-            ("/camera/gyro_accel/sample", "/camera/imu"),
-        ],
-        extra_arguments=[{"use_intra_process_comms": False}],
-        parameters=[
-            {"camera_name": camera_name},
-            {"use_hardware_time": True},
-            {"enable_heartbeat": True},
-            {"enumerate_net_device": enumerate_net_device},
-
-            # Compression
-            {"color.image_raw.enable_pub_plugins": plugins},
-            {"depth.image_raw.enable_pub_plugins": plugins},
-            {"ir.image_raw.enable_pub_plugins": plugins},
-            {".color.image_raw.compressed": compr_jpeg},
-            {".depth.image_raw.compressed": compr_png},
-            {".ir.image_raw.compressed": compr_png},
-
-            # Color
-            {"enable_color": True},
-            {"color_width": 1280},
-            {"color_height": 720},
-            {"color_fps": 30},
-            {"color_format": "MJPG"},
-            {"enable_color_auto_exposure": True},
-            {"enable_color_auto_white_balance": True},
-
-            # Depth
-            {"enable_depth": True},
-            {"depth_width": 640},
-            {"depth_height": 576},
-            {"depth_fps": 30},
-            {"depth_format": "Y16"},
-            {"depth_registration": True},
-            {"align_mode": "SW"},
-            {"enable_frame_sync": True},
-            {"enable_depth_scale": True},
-
-            # IR
-            {"enable_ir": True},
-            {"ir_width": 640},
-            {"ir_height": 576},
-            {"ir_fps": 30},
-            {"ir_format": "Y16"},
-            {"enable_ir_auto_exposure": True},
-
-            # IMU
-            {"enable_gyro": True},
-            {"enable_accel": True},
-            {"accel_rate": "200hz"},
-            {"accel_range": "4g"},
-            {"gyro_rate": "200hz"},
-            {"gyro_range": "1000dps"},
-            {"enable_sync_output_accel_gyro": True},
-
-            # Point cloud
-            {"enable_colored_point_cloud": enable_colored_pc},
-            {"ordered_pc": True},
-        ],
-    )
-
-    rgbd = ComposableNode(
-        package="depth_image_proc",
-        plugin="depth_image_proc::PointCloudXyzrgbNode",
-        name="rgbd",
-        namespace="",
-        extra_arguments=[{"use_intra_process_comms": True}],
-        remappings=[
-            ("depth_registered/image_rect", "/camera/depth/image_raw"),
-            ("rgb/image_rect_color", "/camera/color/image_raw"),
-            ("rgb/camera_info", "/camera/color/camera_info"),
-            ("points", "/camera/points"),
-        ],
-    )
-
-    container = ComposableNodeContainer(
-        name="camera_container",
-        namespace="",
-        package="rclcpp_components",
-        executable="component_container",
-        composable_node_descriptions=[femto, rgbd],
-        emulate_tty=True,
-    )
-
-    return LaunchDescription([
-        declare_camera_name,
-        declare_enable_colored_pc,
-        declare_enumerate_net_device,
-        container,
-    ])
+    # Node configuration
+    parameters = [{arg.name: LaunchConfiguration(arg.name)} for arg in args]
+    # get  ROS_DISTRO
+    ros_distro = os.environ["ROS_DISTRO"]
+    if ros_distro == "foxy":
+        return LaunchDescription(
+            args
+            + [
+                Node(
+                    package="orbbec_camera",
+                    executable="orbbec_camera_node",
+                    name="ob_camera_node",
+                    namespace=LaunchConfiguration("camera_name"),
+                    parameters=parameters,
+                    output="screen",
+                )
+            ]
+        )
+    # Define the ComposableNode
+    else:
+        # Define the ComposableNode
+        compose_node = ComposableNode(
+            package="orbbec_camera",
+            plugin="orbbec_camera::OBCameraNodeDriver",
+            name=LaunchConfiguration("camera_name"),
+            namespace="",
+            parameters=parameters,
+        )
+        # Define the ComposableNodeContainer
+        container = ComposableNodeContainer(
+            name="camera_container",
+            namespace="",
+            package="rclcpp_components",
+            executable="component_container",
+            composable_node_descriptions=[
+                compose_node,
+            ],
+            output="screen",
+        )
+        # Launch description
+        ld = LaunchDescription(
+            args
+            + [
+                GroupAction(
+                    [PushRosNamespace(LaunchConfiguration("camera_name")), container]
+                )
+            ]
+        )
+        return ld
